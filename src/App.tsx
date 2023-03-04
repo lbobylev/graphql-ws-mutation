997e2c4432d996e6657739a3ca1880ec83761218
Leonid Bobylev <leonid.bobylev@surgetech.eu>
2023-03-04 20:50:39 +0100

    use effects

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import { createClient } from "graphql-ws";
 import "./App.css";
 import _ from "lodash";
 
+const client = createClient({
+  url: "ws://localhost:4000/graphql",
+});
+
 function App() {
-  const client = createClient({
-    url: "ws://localhost:4000/graphql",
-  });
+  const [x, setX] = useState("");
+  useEffect(() => {
+    // subscription
+    (async () => {
+      const onNext = (data: any) => {
+        setX(data.data.greetings);
+        /* handle incoming values */
+      };
 
-  // subscription
-  (async () => {
-    const onNext = (data: any) => {
-      console.log(data.data.greetings);
-      /* handle incoming values */
-    };
+      let unsubscribe = () => {
+        /* complete the subscription */
+      };
 
-    let unsubscribe = () => {
-      /* complete the subscription */
-    };
+      await new Promise((resolve: any, reject) => {
+        unsubscribe = client.subscribe(
+          {
+            query: "subscription { greetings }",
+          },
+          {
+            next: onNext,
+            error: reject,
+            complete: resolve,
+          }
+        );
+      });
+    })();
+  }, []);
 
-    await new Promise((resolve: any, reject) => {
-      unsubscribe = client.subscribe(
+  const onChange = _.debounce((e) => {
+    const x = e.target.value;
+    let result;
+    new Promise((resolve, reject) => {
+      let result: any;
+      client.subscribe(
         {
-          query: "subscription { greetings }",
+          query: `mutation { setName(x: "${x}") }`,
         },
         {
-          next: onNext,
+          next: ({ data }) => (result = data),
           error: reject,
-          complete: resolve,
+          complete: () => resolve(result),
         }
       );
     });
-  })();
+  }, 500);
 
   return (
     <div className="App">
-      <input
-        type="text"
-        onChange={_.debounce((e) => {
-          const x = e.target.value;
-          let result;
-          new Promise((resolve, reject) => {
-            let result: any;
-            client.subscribe(
-              {
-                query: `mutation { setName(x: "${x}") }`,
-              },
-              {
-                next: ({ data }) => (result = data),
-                error: reject,
-                complete: () => resolve(result),
-              }
-            );
-          });
-        }, 500)}
-      />
+      <input type="text" onChange={onChange} />
+      <div>{x}</div>
     </div>
   );
 }
